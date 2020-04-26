cdc0a6d255422f52710e33f87a17250ebc105f59
xiaoxiao <caixiaoliang@zto.com>
2020-04-26 10:05:14 +0800

    fix shardMaster client

--- src/shardmaster/client.go
+++ src/shardmaster/client.go
@@ -13,26 +13,26 @@ import "sync"
 type Clerk struct {
 	servers []*labrpc.ClientEnd
 	// Your data here.
-	id int
+	id int64
 	seqnum int
 	LeaderId int
 }
 
-var ClientIdCh chan int
+var ClientIdCh chan int64
 var once  sync.Once
 
 
-func getClientIdCh() chan int {
+func getClientIdCh() chan int64 {
     once.Do(func() {
-        ClientIdCh = make(chan int, 1)
-        max := 1 << 32
+        ClientIdCh = make(chan int64, 1)
+        max := 1 << 64
         go func(){
-            for i :=0;i<max;i++{
-                ClientIdCh <- i
+            for i :=0; i <max;i++ {
+                ClientIdCh <- int64(i)
             }
         }()
     })
-    return ClientIdCh 
+    return ClientIdCh
 }
 
 
@@ -58,31 +58,6 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 
 
 //这里的Call 是同步的，不加超时和重试，如果网络隔离会一直卡住，得不到返回，why？
-// func (ck *Clerk) Query(num int) Config {
-// 	args := &QueryArgs{}
-// 	// Your code here.
-// 	args.Num = num
-// 	args.ClientId = ck.id
-// 	args.SeqNum = ck.seqnum
-// 	for {
-// 		for idx, _ := range ck.servers {
-// 			var reply QueryReply
-// 			DPrintf("Client[%d]:1. send a  Query to leader. LeaderId:%d, ck.seqnum:%d", ck.id, ck.LeaderId,ck.seqnum)
-// 			ok := ck.servers[ck.LeaderId].Call("ShardMaster.Query", args, &reply)
-// 			if ok && reply.WrongLeader == false {
-// 				ck.seqnum += 1
-// 				DPrintf("Client:[%d]:4.Query Client  GET QueryResponse success, num is %d, ck.SeqNo: %d, LeaderId:%d, reply.Config:%v", ck.id, num, ck.seqnum, ck.LeaderId, reply.Config)
-// 				return reply.Config
-// 			}else{
-// 				ck.LeaderId = idx
-// 			}
-			
-// 		}
-// 		time.Sleep(100 * time.Millisecond)
-// 	}
-// }
-
-//这里的Call 是同步的，不加超时和重试，如果网络隔离会一直卡住，得不到返回，why？
 func (ck *Clerk) Query(num int) Config {
 	args := &QueryArgs{}
 	// Your code here.
@@ -92,14 +67,14 @@ func (ck *Clerk) Query(num int) Config {
 	for {
 		for idx, _ := range ck.servers {
 			var reply QueryReply
-			DPrintf("Client[%d]:1. send a  Query to leader. LeaderId:%d, ck.seqnum:%d", ck.id, ck.LeaderId,ck.seqnum)
+			DPrintf("ShardMasterClient[%d]:1.send a  Query to leader. LeaderId:%d, ck.seqnum:%d", ck.id, ck.LeaderId,ck.seqnum)
 			ok := ck.servers[idx].Call("ShardMaster.Query", args, &reply)
 			if ok && reply.WrongLeader == false {
 				ck.seqnum += 1
-				DPrintf("Client:[%d]:4.Query Client  GET QueryResponse success, num is %d, ck.SeqNo: %d, LeaderId:%d, reply.Config:%v", ck.id, num, ck.seqnum, ck.LeaderId, reply.Config)
+				DPrintf("ShardMasterShardMasterClient[%d]:4.Query  Success, num is %d, ck.SeqNo: %d, LeaderId:%d, reply.Config:%v", ck.id, num, ck.seqnum, ck.LeaderId, reply.Config)
 				return reply.Config
 			}else{
-				//ck.LeaderId = idx
+				DPrintf("ShardMasterShardMasterClient[%d]:4.Query  Failed, num is %d, ck.SeqNo: %d, LeaderId:%d, reply.Config:%v", ck.id, num, ck.seqnum, ck.LeaderId, reply.Config)
 			}
 			
 		}
@@ -108,31 +83,39 @@ func (ck *Clerk) Query(num int) Config {
 }
 
 
-// func (ck *Clerk) Join(servers map[int][]string) {
-// 	args := &JoinArgs{}
-// 	// Your code here.
-// 	args.Servers = servers
-// 	args.ClientId = ck.id
-// 	args.SeqNum = ck.seqnum
-
-// 	for {
-// 		// try each known server.
-// 		for idx, _ := range ck.servers {
-// 			var reply JoinReply
-// 			DPrintf("Client[%d]:1. send a  Join to leader. LeaderId:%d, ck.seqnum:%d, servers: %v", ck.id, ck.LeaderId,ck.seqnum,servers)
-// 			ok := ck.servers[ck.LeaderId].Call("ShardMaster.Join", args, &reply)
-// 			if ok && reply.WrongLeader == false {
-// 				ck.seqnum += 1
-// 				DPrintf("Client:[%d]:4.Join Client  GET JoinResponse success, ck.SeqNo: %d, LeaderId:%d", ck.id, ck.seqnum, ck.LeaderId)
-// 				return
-// 			}else{
-// 				ck.LeaderId = idx
-// 			}
-			
-// 		}
-// 		time.Sleep(100 * time.Millisecond)
-// 	}
-// }
+//这里的Call 是同步的，不加超时和重试，如果网络隔离会一直卡住，得不到返回，why？
+//func (ck *Clerk) Query(num int) Config {
+//	args := &QueryArgs{}
+//	// Your code here.
+//	args.Num = num
+//	args.ClientId = ck.id
+//	args.SeqNum = ck.seqnum
+//	for {
+//		for idx, _ := range ck.servers {
+//			workDoneChan := make(chan Config)
+//			go func(){
+//				var reply QueryReply
+//				DPrintf("ShardMasterClient[%d]:1. send a  Query to leader. LeaderId:%d, ck.seqnum:%d", ck.id, ck.LeaderId,ck.seqnum)
+//				ok := ck.servers[idx].Call("ShardMaster.Query", args, &reply)
+//				if ok && reply.WrongLeader == false {
+//					ck.seqnum += 1
+//					DPrintf("Client:[%d]:4.Query Client  GET QueryResponse success, num is %d, ck.SeqNo: %d, LeaderId:%d, reply.Config:%v", ck.id, num, ck.seqnum, ck.LeaderId, reply.Config)
+//					workDoneChan<-reply.Config
+//					//return reply.Config
+//				}
+//			}()
+//			select {
+//			case <-time.After(1000 * time.Millisecond):
+//				goto lable
+//			case resconfig := <-workDoneChan:
+//				return  resconfig
+//			}
+//		lable:
+//			DPrintf("Client:[%d]:5.Query Client  GET timeout")
+//		}
+//		//time.Sleep(100 * time.Millisecond)
+//	}
+//}
 
 func (ck *Clerk) Join(servers map[int][]string) {
 	args := &JoinArgs{}
@@ -145,47 +128,18 @@ func (ck *Clerk) Join(servers map[int][]string) {
 		// try each known server.
 		for idx, _ := range ck.servers {
 			var reply JoinReply
-			DPrintf("Client[%d]:1. send a  Join to leader. LeaderId:%d, ck.seqnum:%d, servers: %v", ck.id, ck.LeaderId,ck.seqnum,servers)
+			DPrintf("ShardMasterClient[%d]:1. send a  Join to leader. LeaderId:%d, ck.seqnum:%d, servers: %v", ck.id, ck.LeaderId,ck.seqnum,servers)
 			ok := ck.servers[idx].Call("ShardMaster.Join", args, &reply)
 			if ok && reply.WrongLeader == false {
 				ck.seqnum += 1
-				DPrintf("Client:[%d]:4.Join Client  GET JoinResponse success, ck.SeqNo: %d, LeaderId:%d", ck.id, ck.seqnum, ck.LeaderId)
+				DPrintf("ShardMasterClient[%d]:4.Join Client  GET JoinResponse success, ck.SeqNo: %d, LeaderId:%d", ck.id, ck.seqnum, ck.LeaderId)
 				return
-			}else{
-				//ck.LeaderId = idx
 			}
 			
 		}
 		time.Sleep(100 * time.Millisecond)
 	}
 }
-
-
-
-// func (ck *Clerk) Leave(gids []int) {
-// 	args := &LeaveArgs{}
-// 	// Your code here.
-// 	args.GIDs = gids
-// 	args.ClientId = ck.id
-// 	args.SeqNum = ck.seqnum
-// 	for {
-// 		// try each known server.
-// 		for idx,_ := range ck.servers {
-// 			var reply LeaveReply
-// 			DPrintf("Client[%d]:1. send a  Leave to leader. LeaderId:%d, ck.seqnum:%d, gids:%v", ck.id, ck.LeaderId,ck.seqnum,gids)
-// 			ok := ck.servers[ck.LeaderId].Call("ShardMaster.Leave", args, &reply)
-// 			if ok && reply.WrongLeader == false {
-// 				ck.seqnum += 1
-// 				DPrintf("Client:[%d]:4.Leave Client  GET LeaveResponse success, ck.SeqNo: %d, LeaderId:%d", ck.id, ck.seqnum, ck.LeaderId)
-// 				return
-// 			}else{
-// 				ck.LeaderId = idx
-// 			}
-			
-// 		}
-// 		time.Sleep(100 * time.Millisecond)
-// 	}
-// }
 
 func (ck *Clerk) Leave(gids []int) {
 	args := &LeaveArgs{}
@@ -197,47 +151,17 @@ func (ck *Clerk) Leave(gids []int) {
 		// try each known server.
 		for idx,_ := range ck.servers {
 			var reply LeaveReply
-			DPrintf("Client[%d]:1. send a  Leave to leader. LeaderId:%d, ck.seqnum:%d, gids:%v", ck.id, ck.LeaderId,ck.seqnum,gids)
+			DPrintf("ShardMasterClient[%d]:1. send a  Leave to leader. LeaderId:%d, ck.seqnum:%d, gids:%v", ck.id, ck.LeaderId,ck.seqnum,gids)
 			ok := ck.servers[idx].Call("ShardMaster.Leave", args, &reply)
 			if ok && reply.WrongLeader == false {
 				ck.seqnum += 1
-				DPrintf("Client:[%d]:4.Leave Client  GET LeaveResponse success, ck.SeqNo: %d, LeaderId:%d", ck.id, ck.seqnum, ck.LeaderId)
+				DPrintf("ShardMasterClient[%d]:4.Leave Client  GET LeaveResponse success, ck.SeqNo: %d, LeaderId:%d", ck.id, ck.seqnum, ck.LeaderId)
 				return
-			}else{
-				//ck.LeaderId = idx
 			}
-			
 		}
 		time.Sleep(100 * time.Millisecond)
 	}
 }
-
-
-// func (ck *Clerk) Move(shard int, gid int) {
-// 	args := &MoveArgs{}
-// 	// Your code here.
-// 	args.Shard = shard
-// 	args.GID = gid
-// 	args.ClientId = ck.id
-// 	args.SeqNum = ck.seqnum
-// 	for {
-// 		// try each known server.
-// 		for idx,_ := range ck.servers {
-// 			var reply MoveReply
-// 			DPrintf("Client[%d]:1. send a  Move to leader. LeaderId:%d, ck.seqnum:%d, shard:%v, gid:%v", ck.id, ck.LeaderId,ck.seqnum, shard, gid)
-// 			ok := ck.servers[ck.LeaderId].Call("ShardMaster.Move", args, &reply)
-// 			if ok && reply.WrongLeader == false {
-// 				ck.seqnum += 1
-// 				DPrintf("Client:[%d]:4.Move Client  GET MoveResponse success, ck.SeqNo: %d, LeaderId:%d", ck.id, ck.seqnum, ck.LeaderId)
-// 				return
-// 			}else{
-// 				ck.LeaderId = idx
-// 			}
-			
-// 		}
-// 		time.Sleep(100 * time.Millisecond)
-// 	}
-// }
 
 func (ck *Clerk) Move(shard int, gid int) {
 	args := &MoveArgs{}
@@ -250,14 +174,12 @@ func (ck *Clerk) Move(shard int, gid int) {
 		// try each known server.
 		for idx,_ := range ck.servers {
 			var reply MoveReply
-			DPrintf("Client[%d]:1. send a  Move to leader. LeaderId:%d, ck.seqnum:%d, shard:%v, gid:%v", ck.id, ck.LeaderId,ck.seqnum, shard, gid)
+			DPrintf("ShardMasterClient[%d]:1. send a  Move to leader. LeaderId:%d, ck.seqnum:%d, shard:%v, gid:%v", ck.id, ck.LeaderId,ck.seqnum, shard, gid)
 			ok := ck.servers[idx].Call("ShardMaster.Move", args, &reply)
 			if ok && reply.WrongLeader == false {
 				ck.seqnum += 1
-				DPrintf("Client:[%d]:4.Move Client  GET MoveResponse success, ck.SeqNo: %d, LeaderId:%d", ck.id, ck.seqnum, ck.LeaderId)
+				DPrintf("ShardMasterClient[%d]:4.Move Client  GET MoveResponse success, ck.SeqNo: %d, LeaderId:%d", ck.id, ck.seqnum, ck.LeaderId)
 				return
-			}else{
-				//ck.LeaderId = idx
 			}
 			
 		}
