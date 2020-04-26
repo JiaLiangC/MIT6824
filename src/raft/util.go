c1a8e2d64838bc11e9efb631c157f7440af180f2
xiaoxiao <caixiaoliang@zto.com>
2020-04-26 10:50:32 +0800

    关闭全局日志开关，修改为按照模块控制开关

--- src/raft/util.go
+++ src/raft/util.go
@@ -1,13 +1,11 @@
 package raft
 
-import "log"
-
 // Debugging
-const Debug = 1
+//const Debug = 1
 
-func DPrintf(format string, a ...interface{}) (n int, err error) {
-	if Debug > 0 {
-		log.Printf(format, a...)
-	}
-	return
-}
+//func DPrintf(format string, a ...interface{}) (n int, err error) {
+//	if Debug > 0 {
+//		log.Printf(format, a...)
+//	}
+//	return
+//}
